7a6b63ead2d5e94f374d71ae4ccb0c3fe06be9ee
aaymenb <aymen1996ben@gmail.com>
2024-11-28 16:10:13 +0000

    aymen

--- print_integer.c
+++ print_integer.c
@@ -1,55 +1,167 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "main.h"
-#include <limits.h>
 
 /**
- * print_int - print a int.
- * @args: arguments.
- *
- * Return: 1
+ * print_int - print an int value
+ * @va: an argument
+ * Return: an integer value;
  */
-
-int print_int(va_list args)
+int print_int(va_list va)
 {
-	int n = va_arg(args, int);
-	int len3 = 0, temp = n, digits[12];
-	int i = 0, j;
+	int n = va_arg(va, int), n_digit = 0, i = 0;
+	char buffer[1024];
 
-	if (n == INT_MIN)
-	{
-		char int_min_str[] = "-2147483648";
-
-		for (j = 0; int_min_str[j] != '\0'; j++)
-		{
-			_putchar(int_min_str[j]);
-		}
-		return (11);
-	}
-	if (n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
 	if (n < 0)
 	{
+		n_digit++;
 		_putchar('-');
 		n = -n;
-		len3++;
 	}
-	while (temp != 0)
+	while (i < 1024)
 	{
-		temp /= 10;
-		len3++;
-	}
-	while (n != 0)
-	{
-		digits[i++] = n % 10;
+		if (n > 0)
+			buffer[i] = ('0' + (n % 10));
+		else
+			buffer[i] = ('0' - (n % 10));
+
 		n /= 10;
+		if (n == 0)
+			break;
+		i++;
 	}
-	for (j = i - 1; j >= 0; j--)
+	while (i >= 0)
 	{
-		_putchar(digits[j] + '0');
+		_putchar(*(buffer + i));
+		n_digit++;
+		i--;
 	}
-	return (len3);
+
+	return (n_digit);
+}
+
+/**
+ * print_unsigned_int - print an unsigned int
+ * @va: an argument
+ * Return: an integer
+ */
+int print_unsigned_int(va_list va)
+{
+	unsigned int n = va_arg(va, unsigned int);
+	int n_digit = 0, i = 0;
+	char buffer[1024];
+
+	while (i < 1024)
+	{
+		buffer[i] = ('0' + (n % 10));
+		n /= 10;
+		if (n == 0)
+			break;
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(*(buffer + i));
+		n_digit++;
+		i--;
+	}
+	return (n_digit);
+}
+
+/**
+ * print_unsigned_octal - print octal value
+ * @va: argument value
+ * Return: number of digits;
+ */
+int print_unsigned_octal(va_list va)
+{
+	unsigned int n = va_arg(va, unsigned int);
+	int n_digit = 0, i = 0;
+	char buffer[1024];
+
+	while (i < 1024)
+	{
+		buffer[i] = ('0' + (n % 8));
+		n /= 8;
+		if (n == 0)
+			break;
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(*(buffer + i));
+		n_digit++;
+		i--;
+	}
+	return (n_digit);
+}
+
+/**
+ * print_unsigned_hexadecimal - function to
+ * print hexadecimal value
+ * @va: argumant value
+ * Return: integer
+ */
+int print_unsigned_hexadecimal(va_list va)
+{
+	unsigned int n = va_arg(va, unsigned int);
+	int n_digit = 0, i = 0, r = 0;
+	char buffer[1024];
+
+	while (i < 1024)
+	{
+		r = n % 16;
+		if (r < 10)
+			buffer[i] = '0' + r;
+		else
+			buffer[i] = 'a' + (r - 10);
+
+		n /= 16;
+		if (n == 0)
+			break;
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(*(buffer + i));
+		n_digit++;
+		i--;
+	}
+	return (n_digit);
+}
+
+/**
+ * print_unsigned_Hexadecimal - function to
+ * print hexadecimal value
+ * @va: argumant value
+ * Return: integer
+ */
+int print_unsigned_Hexadecimal(va_list va)
+{
+	unsigned int n = va_arg(va, unsigned int);
+	int n_digit = 0, i = 0, r = 0;
+	char buffer[1024];
+
+	while (i < 100)
+	{
+		r = n % 16;
+		if (r < 10)
+			buffer[i] = '0' + r;
+		else
+			buffer[i] = 'A' + (r - 10);
+
+		n /= 16;
+		if (n == 0)
+			break;
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(*(buffer + i));
+		n_digit++;
+		i--;
+	}
+	return (n_digit);
 }
