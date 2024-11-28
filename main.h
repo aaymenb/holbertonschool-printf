279ebce4b9b6233013a61c4cfd426d6bdadda3b5
aaymenb <aymen1996ben@gmail.com>
2024-11-28 22:32:13 +0100

    aymen

--- main.h
+++ main.h
@@ -1,35 +1,34 @@
-#ifndef MAIN_HEADER
-#define MAIN_HEADER
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * struct toprint - type of argument to print/ specifier letter
- *  and pointer to func
- * @letter: type of specifier letter (format[i +1]) example d s c
- * @f: function that is associated with the specifier letter ^^
+ * struct format_sp - Struct for format specifiers and
+ * their corresponding functions
+ * @specifi: The format specifier character
+ * ('c' for char, 's' for string)
+ * @print_func: Function pointer to
+ * the function that prints the corresponding type
  */
 
-/* -----------little typedef--------- */
-
-typedef struct toprint
+typedef struct format_sp
 {
-	char *letter;
-	int (*f)(va_list args);
-} toprint_;
+	char specifi;
+	int (*print_func)(va_list args);
+} sp_t;
 
-/* -------all functions used-------- */
-
-int print_c(va_list args);
+int print_char(va_list args);
+int print_string(va_list args);
+int printf(const char *format, ...);
+int print_percent(va_list args);
+int _putchar(char c);
 int _printf(const char *format, ...);
-int print_s(va_list args);
-int print_int(va_list args);
-int print_deci(va_list args);
-int print_purcent(__attribute__((unused)) va_list args);
-int _putchar(char);
-int selection(va_list args, char specifiers);
-int print_id(int n);
+int print_non(char num1, char num2);
+int print_int(va_list ap);
+int print_dec(va_list ap);
 
 #endif
