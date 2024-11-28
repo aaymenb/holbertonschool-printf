8a26aa6ff7ea1959d84543d416fd470ac7e85e17
aaymenb <aymen1996ben@gmail.com>
2024-11-28 23:07:28 +0100

    aymen

--- printf.c
+++ printf.c
@@ -1,66 +1,84 @@
 #include <stdarg.h>
 #include <unistd.h>
+#include "main.h"
 
 /**
- * _printf - Implémentation simplifiée de printf.
- * @format: La chaîne de format contenant les spécificateurs.
+ * print_format - Finds the corresponding function for a given format specifier
+ * @format: The format specifier to match
  *
- * Return: Le nombre de caractères imprimés.
+ * Return: A pointer to the corresponding functionor NULL if no match is found
  */
+
+static int (*print_format(const char *format))(va_list)
+{
+	int jindex = 0;
+
+	sp_t specifiers[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'d', print_dec},
+		{'i', print_int},
+		{'\0', NULL}
+
+	};
+
+	while (specifiers[jindex].specifi)
+	{
+		if (specifiers[jindex].specifi == *format)
+		{
+			return (specifiers[jindex].print_func);
+		}
+		jindex++;
+	}
+
+	return (NULL);
+}
+
+/**
+ * _printf - Custom implementation of printf function
+ * @format: The format string containing format specifiers
+ * @...: The values to format and print
+ *  Return: The total number of characters printed
+ */
+
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int index = 0;
+	int count = 0;
+	int (*print_func)(va_list);
 
-	int count = 0; /* Compteur des caractères imprimés */
-	int i, j; /* Déclarer les variables i et j avant les boucles */
+	if (!format)
+		return (-1);
 
-	/* Initialiser la liste d'arguments */
 	va_start(args, format);
 
-	/* Parcourir la chaîne de format */
-	for (i = 0; format[i] != '\0'; i++) {
-		if (format[i] == '%') {
-			/* Si on trouve un '%', on regarde ce qui suit */
-			i++; /* Passer au caractère suivant */
-
-			if (format[i] == 'c') {
-				/* Spécificateur pour un caractère */
-				char c = va_arg(args, int); /* Récupérer le caractère */
-				write(1, &c, 1); /* Imprimer le caractère */
-				count++; /* Augmenter le compteur */
+	while (format && format[index])
+	{
+		if (format[index] == '%')
+		{
+			index++;
+			if (format[index] == '\0')
+			{
+				return (-1);
 			}
-			else if (format[i] == 's') {
-				/* Spécificateur pour une chaîne de caractères */
-				char *str = va_arg(args, char*);
 
-				/* Si la chaîne est NULL, afficher "(null)" */
-				if (str == NULL) {
-					str = "(null)";
-				}
-
-				/* Imprimer la chaîne caractère par caractère */
-				for (j = 0; str[j] != '\0'; j++) {
-					write(1, &str[j], 1);
-					count++; /* Compter les caractères */
-				}
+			print_func = print_format(&format[index]);
+			if (print_func)
+			{
+				count = count + print_func(args);
 			}
-			else if (format[i] == '%') {
-				/* Si on trouve '%%', afficher un '%' littéral */
-				write(1, "%", 1);
-				count++; /* Compter le caractère '%' */
+			else
+			{
+				count = count + print_non('%', format[index]);
 			}
 		}
-		else {
-			/* Si ce n'est pas un '%', on imprime le caractère tel quel */
-			write(1, &format[i], 1);
-			count++; /* Compter les caractères */
-		}
+		else
+			count = count + write(1, &format[index], 1);
+
+		index++;
 	}
-
-	/* Nettoyer la liste d'arguments */
 	va_end(args);
-
-	/* Retourner le nombre de caractères imprimés */
 	return (count);
 }
-
