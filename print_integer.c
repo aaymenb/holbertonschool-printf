f77d8c3a44c3ff62e9eb9ee4638fc35e4b760d30
aaymenb <aymen1996ben@gmail.com>
2024-11-28 16:05:19 +0000

    aymen

--- print_integer.c
+++ print_integer.c
@@ -1,48 +1,55 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include "main.h"
+#include <limits.h>
 
 /**
- * printnumber - function that prints an integer
- * @n: integer to print
- * Return: number of characters printed
+ * print_int - print a int.
+ * @args: arguments.
+ *
+ * Return: 1
  */
-int printnumber(int n)
+
+int print_int(va_list args)
 {
-	int count = 0;
+	int n = va_arg(args, int);
+	int len3 = 0, temp = n, digits[12];
+	int i = 0, j;
 
-	if (n == -2147483648)
+	if (n == INT_MIN)
 	{
-		_putchar('-');
-		_putchar('2');
-		n = 147483648;
-		count += 2;
-	}
+		char int_min_str[] = "-2147483648";
 
+		for (j = 0; int_min_str[j] != '\0'; j++)
+		{
+			_putchar(int_min_str[j]);
+		}
+		return (11);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
-		count++;
+		len3++;
 	}
-
-	if (n / 10 != 0)
+	while (temp != 0)
 	{
-		count += printnumber(n / 10);
+		temp /= 10;
+		len3++;
 	}
-
-	_putchar(n % 10 + '0');
-	count++;
-
-	return (count);
-}
-
-/**
- * print_integer - function that prints an integer from a va_list
- * @arguments: va_list containing the integer to print
- * Return: number of characters printed
- */
-int print_integer(va_list arguments)
-{
-	int number = va_arg(arguments, int);
-
-	return (printnumber(number));
+	while (n != 0)
+	{
+		digits[i++] = n % 10;
+		n /= 10;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(digits[j] + '0');
+	}
+	return (len3);
 }
